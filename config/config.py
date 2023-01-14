05e75a5183a6722c093f275d3092716cd3ae3eb2
˹ʀσᴍᴇσ ꭙ˼ <109813393+RomeoX1025@users.noreply.github.com>
2023-01-15 01:25:00 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -47,7 +47,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Romeo X1025/TrixieMusic",
+    "https://github.com/RomeoX1025/TrixieXMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
